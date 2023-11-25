4710d15256bf509e957ce04be2c10331b123ebee
chaitraboggaram <113207424+chaitraboggaram@users.noreply.github.com>
2023-11-24 19:37:13 -0800

    Latest fixes on code
    Fixing unsubscribe, added 3 additional coins, updated server to publish multiple messages and also stop feature, minor styling updates and added README.md, implemented HTTP server in client and removed socket

--- pubsubClient.py
+++ pubsubClient.py
@@ -1,9 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 from jsonrpclib import Server
 import socket
 import threading
 import json
 import time
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 HOST = "localhost"
 PORT = 8000  # default
@@ -11,90 +16,96 @@ data = {}
 listener_track = {}
 
 class Listener(threading.Thread):
-    def __init__(self, channel_name, thread_name):
+    def __init__(self, channel_name, listener_name):
         threading.Thread.__init__(self)
         self.channel_name = channel_name
-        self.broker = Server('http://localhost:1006')
-        self.thread_name = thread_name
-        self.message_queue = self.broker.subscribe(thread_name, channel_name)
+        self.broker = Server('http://localhost:5000')
+        self.thread_name = listener_name
+        self.message_queue = self.broker.subscribe(listener_name, channel_name)
         self.daemon = True  # Ensure thread exits when main program does
+        self._stop_event = threading.Event()
 
     def run(self):
         print(f"{self.thread_name} Run start, listening to messages on channel {self.channel_name}")
-        is_running = True
-        while is_running:
+        global data
+        while not self._stop_event.is_set():
             message = self.broker.listen(self.thread_name, self.channel_name)
             if message:
-                print(f"Received message: {message}")
+                if self.thread_name not in data:
+                    data[self.thread_name] = {}
+                data[self.thread_name][self.channel_name] = message
                 # Process the message here
             else:
                 # No message in queue, sleep for a short time to avoid busy waiting
-                time.sleep(0.1)
+                time.sleep(0.5)
+                self._stop_event.wait(timeout=0.5)
+
+    def stop(self):
+        self._stop_event.set()
 
     def unsubscribe(self):
-        self.broker.unsubscribe__(self.thread_name, self.channel_name)
+        self.broker.unsubscribe(self.thread_name, self.channel_name)
 
-class ThreadedServer(object):
-    def __init__(self):
-        print("Client started")
 
-    def listen(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind((HOST, PORT))
-            sock.listen()
-            while True:
-                conn, addr = sock.accept()
-                t = threading.Thread(target=self.handle_client, args=(conn, addr))
-                t.start()
+class Handler(BaseHTTPRequestHandler):
+    global data
+    global listener_track
 
-    def handle_client(self, client, address):
-        size = 1024
-        with client:
-            client.settimeout(5)
-            while True:
-                try:
-                    global listener_track
-                    request = client.recv(size).decode()
-                    headers = request.split('\r\n')
-                    REST = headers[0].split()
-                    if "/subscribe" in REST[1]:
-                        query = REST[1].split('?')[1]
-                        params = dict(x.split('=') for x in query.split('&'))
-                        listener_name = params['listener']
-                        channel_name = params['channel']
-                        listener = Listener(channel_name, listener_name)
-                        if listener_name not in listener_track:
-                            listener_track[listener_name] = {}
-                        listener_track[listener_name][channel_name] = listener
-                        listener.start()
-                    elif "/getData" in REST[1]:
-                        query = REST[1].split('?')[1]
-                        params = dict(x.split('=') for x in query.split('&'))
-                        listener_name = params['listener']
-                        self.send_data(client, listener_name)
-                    elif "/unsubscribe" in REST[1]:
-                        query = REST[1].split('?')[1]
-                        params = dict(x.split('=') for x in query.split('&'))
-                        listener_name = params['listener']
-                        channel_name = params['channel']
-                        if listener_name in listener_track and channel_name in listener_track[listener_name]:
-                            listener_track[listener_name][channel_name].unsubscribe()
-                except Exception as e:
-                    break
-            client.close()
+    def do_GET(self):
+        try:
+            parsed_url = urlparse(self.path)
+            path = parsed_url.path
+            query_params = parse_qs(parsed_url.query)
 
-    def send_data(self, client, listener_name):
-        global data
-        json_string = json.dumps(data.get(listener_name, []))
-        client.sendall(str.encode("HTTP/1.1 200 OK\n", 'iso-8859-1'))
-        client.sendall(str.encode('Content-Type: application/json\n', 'iso-8859-1'))
-        client.sendall(str.encode('Access-Control-Allow-Origin: *\n', 'iso-8859-1'))
-        client.sendall(str.encode('\r\n'))
-        client.sendall(json_string.encode())
+            if path == '/subscribe':
+                listener_name = query_params['listener'][0]
+                channel_name = query_params['channel'][0]
+                listener = Listener(channel_name, listener_name)
+                if listener_name not in listener_track:
+                    listener_track[listener_name] = {}
+                listener_track[listener_name][channel_name] = listener
+                listener.start()
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
 
-def main():
-    ThreadedServer().listen()
+            elif path == "/getData":
+                listener_name = query_params['listener'][0]
+                channel_name = query_params['channel'][0]
+                if data.get(listener_name):
+                    response_data = data.get(listener_name).get(channel_name, [])
+                    json_string = json.dumps([response_data]) if response_data != [] else json.dumps(response_data)
+                else:
+                    json_string = json.dumps([])
+                print(f"Response JSON: {json_string}")
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+
+                self.wfile.write(json_string.encode('utf-8'))
+
+            elif path == "/unsubscribe":
+                listener_name = query_params['listener'][0]
+                channel_name = query_params['channel'][0]
+                if listener_name in listener_track and channel_name in listener_track[listener_name]:
+                    listener_track[listener_name][channel_name].stop()
+                    listener_track[listener_name][channel_name].join()
+                    listener_track[listener_name][channel_name].unsubscribe()
+
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+
+        except Exception as e:
+            # pass
+            logger.error("Caught an Exception", e)
+
 
 if __name__ == '__main__':
-    main()
+    # main()
+    with HTTPServer(('', PORT), Handler) as server:
+        print(f"HTTP Server Running on port {PORT}...")
+        server.serve_forever()
